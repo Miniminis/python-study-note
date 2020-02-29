5d45442c8aa64063f5aa0b98e9fecf705ae20d4b
minhee.son <44446425+Miniminis@users.noreply.github.com>
2020-02-29 13:53:43 +0900

    delete board

--- blog-project/doc.py
+++ blog-project/doc.py
@@ -1,3 +1,4 @@
+# django project 세팅 
 # 1. mkdir, cd dir
 # 2. pip freeze 
 # 3. pip install virtualenv
@@ -7,4 +8,13 @@
 # 7. pip install django 
 # 8. django-admin startproject [] - 전체 프로젝트 
 # 9. django-admin startapp [] - 기능단위
-# 10. python manage.py migrate - dbsqlite3 생성됨 : 로컬에서 쓸 수 있는 데이터 베이스가 생성됨
+# 10. python manage.py migrate - dbsqlite3 생성됨 : 로컬에서 쓸 수 있는 데이터 베이스가 생성됨 - 깃에서 업로드 제외
+
+
+# MTV 구조 
+# 1. blog urls 설정 - include  
+# 2. feed urls 설정 - views 와 연결 
+# 3. Views : render 페이지 설정 및 비즈니스 로직 
+# 4. Templates : html page 
+# 5. Models : db table 요소 및 구조 세팅 
+
