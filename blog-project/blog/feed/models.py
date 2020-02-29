5edb7f26cd8d70e80b0534bdf30de89409e3cec4
minhee.son <44446425+Miniminis@users.noreply.github.com>
2020-02-29 13:57:38 +0900

    MTV feed 연결

---
+++ blog-project/blog/feed/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+# blog db 구조 setting 
+# 1. article 
+# 2. comment 
+# 3. hashtag 
+
+class Article(models.Model):
+    CATEGORY_CHOICES = [
+        ('DV', 'Development'),
+        ('PS', 'Personal'),
+        ('AF', 'Africa'),
+    ]
+    title = models.CharField(max_length=200, verbose_name="제목")
+    content = models.TextField(verbose_name="본문내용")
+    category = models.CharField(
+        max_length=3,
+        choices = CATEGORY_CHOICES,
+        default = ('DV', 'Development'),
+        verbose_name="카테고리"
+    )
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    username = models.CharField(max_length=50, verbose_name="댓글작성자")
+    content = models.CharField(max_length=200, verbose_name="댓글내용")
+
+   
+class HashTag(models.Model):
+    hashtag = models.CharField(max_length=50, verbose_name="해쉬태그")
