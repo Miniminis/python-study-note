695d04f46acc09cb512070f57ca317e8b03060d6
minhee.son <44446425+Miniminis@users.noreply.github.com>
2020-02-29 12:24:59 +0900

    blog project : 새 프로젝트 생성 및 board app 추가, db migration 진행 완료

---
+++ blog-project/doc.py
@@ -0,0 +1,10 @@
+# 1. mkdir, cd dir
+# 2. pip freeze 
+# 3. pip install virtualenv
+# 4. python -m venv sample_env
+# 5. sample_env\scripts\activate
+# 6. pip freeze 
+# 7. pip install django 
+# 8. django-admin startproject [] - 전체 프로젝트 
+# 9. django-admin startapp [] - 기능단위
+# 10. python manage.py migrate - dbsqlite3 생성됨 : 로컬에서 쓸 수 있는 데이터 베이스가 생성됨
