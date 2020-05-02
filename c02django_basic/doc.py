b27c2c3fbb253d5f5d5990c1126283542411db7a
minhee.son <44446425+Miniminis@users.noreply.github.com>
2020-05-02 15:11:14 +0900

    불필요한 프로젝트 삭제

--- c02django_basic/doc.py
+++ c02django_basic/doc.py
@@ -1,12 +1,11 @@
 """
 1. django framework 
 - mvt : model, view, template layer 
+- model : 데이터베이스와 연관 
+- view : business logic
+- template : html codes  
 - form 
 - admin 
 - security and etc.
-
-* model 
-- 데이터베이스와 연관 
-- 
 
 """
