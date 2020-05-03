cb2ed3f59b1ea85075349e054d945ce37e58c907
minhee.son <44446425+Miniminis@users.noreply.github.com>
2020-05-03 17:40:22 +0900

    register : model, view, template 연결 완료. validation check 도 일부 추가

--- c02django_basic/new_community/new_user/views.py
+++ c02django_basic/new_community/new_user/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from .models import NewUser
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
+def register(request):
+    if request.method == "GET":
+        return render(request, 'register.html')
+    elif request.method == "POST":
+        # userEmail = request.POST['userEmail']   
+        # userPw = request.POST['userPw']
+        # userRePw = request.POST['userRePw']
+
+        # if userPw != userRePw:
+        #     return HttpResponse('비밀번호가 다릅니다! ')
+
+        userEmail = request.POST.get('userEmail', None)   
+        userPw = request.POST.get('userPw', None)
+        userRePw = request.POST.get('userRePw', None)
+
+        res_data = {}
+
+        if not (userEmail and userPw and userRePw):
+            res_data['error'] = '모든 값을 입력해야합니다!'
+        elif userPw != userRePw:
+            res_data['error'] = '비밀번호가 다릅니다!'
+        else:
+            newuser = NewUser(
+                username=userEmail,
+                password=make_password(userPw)
+            )
+
+            newuser.save() 
+
+        return render(request, 'register.html', res_data)
