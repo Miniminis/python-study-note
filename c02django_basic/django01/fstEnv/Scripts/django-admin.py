904fb078e8bc5417d5d2f0eb163bf9245097b438
minhee.son <44446425+Miniminis@users.noreply.github.com>
2020-05-14 06:37:19 +0900

    delete django01 example dir

--- c02django_basic/django01/fstEnv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\minhe\documents\github\pythonstudynote\c02django_basic\django01\fstenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ c03flask-basic/doc.py
@@ -0,0 +1,33 @@
+"""
+# 1. 가상환경 설정
+- pip install virtualenv
+- python -m venv []
+- []\scripts\activate
+
+# 2. flask 설치
+- pip freeze 
+- pip install flask 
+- pip freeze > requirements.txt
+
+# 3. flask app 작성해보기 
+- app.py 
+
+MVC Pattern 
+Model(DB와 연결) - View(클라이언트가 보는 화면) - Controller(접근 url 별로 비즈니스 로직 수행)
+
+# SQLAlchemy in Flask 
+- pip install 
+- ORM (Orbject Relational Mapping) : 객체와 DB를 자동으로 매핑해 주는 것
+- python app.py
+- sqlite3 db.sqlite : sqlite 내부 db 확인
+
+# Persistance Framework 
+영속성 : 데이터를 생성한 프로그램이 종료되더라도 데이터가 사라지지않고 저장되는 것
+종류 : SQL Mapper 와 ORM 
+SQL Mapper : Mybatis,  
+ORM : JPA, Hibernate 등 
+
+# 참고 : 
+- [[DB] ORM이란](https://gmlwjd9405.github.io/2019/02/01/orm.html)
+"""
+
