5740f1e9a575830d4cf39b88c5f7b8f70c2d4c18
mhson <mhson@crepass.com>
2020-07-15 21:25:10 +0900

    python datatypes + practice

---
+++ c01basic/sec04-3.py
@@ -0,0 +1,128 @@
+# Section04-3
+# 파이썬 데이터 타입(자료형)
+# 리스트, 튜플
+
+# 리스트 자료형(순서O, 중복O, 수정O, 삭제O)
+
+# 선언
+a = []
+b = list()  # 명시적
+c = [1, 2, 3, 4]
+d = [10, 100, 'Pen', 'Cap', 'Plate']
+e = [10, 100, ['Pen', 'Cap', 'Plate']]
+
+# 인덱싱
+print('#=====#')
+print('d - ', type(d), d)
+print('d - ', d[1])
+print('d - ', d[0] + d[1] + d[1])
+print('d - ', d[-1])
+print('e - ', e[-1][1])
+# print('e - ', e[-1][1][4])
+print('e - ', list(e[-1][1]))
+
+# 슬라이싱
+print('#=====#')
+print('d - ', d[0:3])
+print('d - ', d[2:])
+print('e - ', e[2][1:3])
+
+# 리스트 연산
+print('#=====#')
+print('c + d - ', c + d)
+print('c * 3 - ', c * 3)
+# print("c[0] + 'hi' - ",c[0] + 'hi')
+print("'hi' + c[0] - ", 'hi' + str(c[0]))
+
+# 리스트 수정, 삭제
+print('#=====#')
+c[0] = 4
+print('c - ', c)
+c[1:2] = ['a', 'b', 'c']
+print('c - ', c)
+c[1] = ['a', 'b', 'c']
+print('c - ', c)
+c[1:3] = []
+print('c - ', c)
+del c[3]
+print('c - ', c)
+
+# 리스트 함수
+a = [5, 2, 3, 1, 4]
+
+print('a - ', a)
+a.append(6)
+print('a - ', a)
+a.sort()
+print('a - ', a)
+a.reverse()
+print('a - ', a)
+print('a - ', a.index(5))
+a.insert(2, 7)      #insert 는 원하는 index 에 원하는 값을 삽입, append는 무조건 끝에다가 삽입
+print('a - ', a)
+a.reverse()
+a.remove(1)
+print('a - ', a)
+print('a - ', a.pop())
+print('a - ', a.pop())
+print('a - ', a)
+print('a - ', a.count(4))
+ex = [8, 9]
+a.extend(ex)    # list 를 extend. append는 해당 list 자체를 삽입하고, extend 는 리스트의 한 요소로서 삽입한다. 
+print('a - ', a)
+
+# 삭제 remove, pop, del
+# remove() : 원하는 값을 삭제 
+# del c[1] : 해당 index 의 값을 삭제 
+# pop() : 맨 마지막의 원소를 꺼내서 없엔다. 
+# - stack (LIFO) / pop, push
+
+
+# 반복문 활용
+while a:
+    l = a.pop()
+    print(2 is l)
+
+
+
+
+
+# 튜플 자료형(순서O, 중복O, 수정X,삭제X)
+# 프로그램에 영향을 끼치는 중요한 키값들의 경우, 수정과 삭제를 방지하기 위해서 튜플 자료형으로 세팅한다. 
+
+# 선언
+a = ()
+b = (1,)
+c = (1, 2, 3, 4)
+d = (10, 100, 'Pen', 'Cap', 'Plate')
+e = (10, 100, ('Pen', 'Cap', 'Plate'))
+
+# 인덱싱
+print('#=====#')
+print('d - ', type(d), d)
+print('d - ', d[1])
+print('d - ', d[0] + d[1] + d[1])
+print('d - ', d[-1])
+print('e - ', e[-1][1])
+print('e - ', e[-1][1][4])
+print('e - ', list(e[-1][1]))
+
+# 슬라이싱
+print('#=====#')
+print('d - ', d[0:3])
+print('d - ', d[2:])
+print('e - ', e[2][1:3])
+
+# 튜플 연산
+print('#=====#')
+print('c + d - ', c + d)
+print('c * 3 - ', c * 3)
+# print("c[0] + 'hi' - ",c[0] + 'hi')
+print("'hi' + c[0] - ", 'hi' + str(c[0]))
+
+# 튜플 함수
+a = (5, 2, 3, 1, 4)
+
+print('a - ', a)
+print('a - ', a.index(5))
+print('a - ', a.count(4))
